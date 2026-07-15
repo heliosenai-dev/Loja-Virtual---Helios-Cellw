71f2a99abfd93afc03b6e7d4564107f18247d0ec
Helio Marcos <h.paixao@aluno.senai.br>
2026-07-15 13:01:37 -0300

    produtos alocados

---
+++ js/manta_tela_carinho.js
@@ -0,0 +1,43 @@
+import { listItens, removeItem } from "./carrinho.js";
+
+//MONTANDO A TELA CARRINHO
+const montaTelaCarrinho = () => {
+    //PEGANDO ELEMENTOS DO DOM
+    const sectionItensCarrinho = document.querySelector('#itens-carrinho')
+
+    sectionItensCarrinho.innerHTML = ''
+
+    listItens().forEach((elem, i) => {
+        const sectionItem = document.createElement('section')
+        sectionItem.setAttribute('class', 'item')
+        sectionItem.innerHTML = `<img src='${elem.caminho_da_imagem}' alt=${elem.descricao_produto} class='img-item'/> 
+        <p class='descricao'>${elem.descricao_produto}</p> 
+        <p class='vlr-unitario'>${elem.valor_unitario}</p> 
+        <input type="number" name='quant${i}' id='quant${i}' class="input-item" value=${1}> 
+        <p class="tot-item">${elem.valor_unitario * 1}</p>`
+
+        const imgRemover = document.createElement('img')
+        imgRemover.setAttribute('src','../imagens/icones/remover.png')
+        imgRemover.setAttribute('alt', 'Remover')
+        imgRemover.setAttribute('class', 'img-remover')
+
+        imgRemover.addEventListener('click',()=>{
+            if(confirm(`Deseja remover ${elem.descricao_produto} da sua lista? `)){
+                removerItemCarrinho(i)
+            }
+            
+        })
+        
+        sectionItem.appendChild(imgRemover)
+        
+        sectionItensCarrinho.appendChild(sectionItem)
+    });
+}
+
+const removerItemCarrinho = (pos)=>{
+    removeItem(pos)
+
+    montaTelaCarrinho()
+}
+
+montaTelaCarrinho()
